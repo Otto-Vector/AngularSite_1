9cafde2735d768727bfde3f6a24f8ef850c6980d
Otto-Vector <50682440+Otto-Vector@users.noreply.github.com>
2021-07-29 01:59:53 +0300

    customers & svg content

--- src/app/ui/customers/customers.component.ts
+++ src/app/ui/customers/customers.component.ts
@@ -1,12 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Customers {
+  title: string
+  url: string
+}
+
 @Component({
   selector: 'app-customers',
   templateUrl: './customers.component.html',
-  styleUrls: ['./customers.component.css']
+  styleUrls: ['./customers.component.scss']
 })
 export class CustomersComponent implements OnInit {
 
+  customers : string = 'customers'
+  customers_item : string = this.customers+'__item'
+
+  customersArr : Customers[] = [{
+    title : 'Bitbucket',
+    url: '#bitbucket'
+  },{
+    title: 'AppleWatch',
+    url: '#applewatch'
+  },{
+    title: 'Facebook',
+    url: '#facebook'
+  },{
+    title: 'Atlassian',
+    url: '#atlassian'
+  },{
+    title: 'Audi',
+    url: '#audi'
+  }
+
+  ]
   constructor() { }
 
   ngOnInit(): void {
